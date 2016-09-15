1f634bc99f4dc79a5029fb47342b5ebbc7e98ab2
Rafael Nascimento <rafaeln.dev@gmail.com>
2016-09-15 19:11:59 -0300

    Formatado arquivo de corrigir provas e adicionado export csv

--- corrigir_provas.sh
+++ corrigir_provas.sh
@@ -4,22 +4,72 @@
 find "$(pwd)/rawscans/" > scan-list.txt
 
 # getimages
-auto-multiple-choice getimages --list "$PWD/scan-list.txt" --progression-id analyse --vector-density 250 --copy-to "$PWD/scans" --orientation portrait --debug /tmp/getimages.log
+auto-multiple-choice getimages \
+  --list "$PWD/scan-list.txt" \
+  --progression-id analyse \
+  --vector-density 250 \
+  --copy-to "$PWD/scans" \
+  --orientation portrait
 
 # analyze
-auto-multiple-choice analyse --no-multiple --tol-marque "0.2,0.2" --prop 0.8 --bw-threshold 0.6 --progression-id analyse --progression 1 --n-procs "0" --data data --projet ./ --cr cr --liste-fichiers scan-list.txt --no-ignore-red
+auto-multiple-choice analyse \
+  --no-multiple \
+  --tol-marque "0.2,0.2" \
+  --prop 0.8 \
+  --bw-threshold 0.6 \
+  --progression-id analyse \
+  --progression 1 \
+  --n-procs "0" \
+  --data data \
+  --projet ./ \
+  --cr cr \
+  --liste-fichiers scan-list.txt \
+  --no-ignore-red
 
 # prepare
-auto-multiple-choice prepare --n-copies 3 --with lualatex --filter latex --filtered-source DOC-filtered.tex --progression-id bareme --progression 1 --data data --mode b groups.tex
+auto-multiple-choice prepare \
+  --n-copies 3 \
+  --with lualatex \
+  --filter latex \
+  --filtered-source DOC-filtered.tex \
+  --progression-id bareme \
+  --progression 1 \
+  --data data \
+  --mode b groups.tex
 
 # note
-auto-multiple-choice note --data data --seuil 0.15 --grain 0.5 --arrondi inf --notemax 20 --plafond --notemin "" --postcorrect-student "" --postcorrect-copy "" --progression-id notation --progression 1
+auto-multiple-choice note \
+  --data data \
+  --seuil 0.15 \
+  --grain 0.5 \
+  --arrondi inf \
+  --notemax 20 \
+  --plafond \
+  --notemin "" \
+  --postcorrect-student "" \
+  --postcorrect-copy "" \
+  --progression-id notation \
+  --progression 1
 
 # association-auto
-auto-multiple-choice association-auto --data data --notes-id "<preassoc>" --liste arquivos/alunos.csv --liste-key id --csv-build-name "(nom|surname) (prenom|name)" --encodage-liste UTF-8 --pre-association
+auto-multiple-choice association-auto \
+  --data data \
+  --notes-id "<preassoc>" \
+  --liste arquivos/alunos.csv \
+  --liste-key id \
+  --csv-build-name "(nom|surname) (prenom|name)" \
+  --encodage-liste UTF-8 \
+  --pre-association
 
-# export
+# annote
 auto-multiple-choice annote --xmlargs annote.xml
 
 # regroupe
 auto-multiple-choice regroupe --xmlargs regroupe.xml
+
+# export
+auto-multiple-choice export \
+  --data data \
+  --module csv \
+  --fich-noms arquivos/alunos.csv \
+  --o exports/resultados.csv
